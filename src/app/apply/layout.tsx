ba544d6384475c1c52ccb46e2d2ca9f0dd425527
Mingeon Kim <issac4892@gmail.com>
2024-03-17 01:00:40 +0900

    Update layout.tsx

--- src/app/apply/layout.tsx
+++ src/app/apply/layout.tsx
@@ -2,13 +2,13 @@ import ApplyClientPage from "./page";
 
 export const metadata = {
   title: '지원하기 - KSA Innovation Challenge 2024',
-  description: 'KIC, Where passion meets purpose.',
+  description: 'KSA Innovation Challenge 2024에 지원하세요.',
   openGraph: {
     siteName: 'KSA Innovation Challenge 2024',
     title: '지원하기 - KSA Innovation Challenge 2024',
     description: 'KSA Innovation Challenge 2024에 지원하세요.',
     type: 'website',
-    url: 'https://innovation-challenge.cloud.algorix.io',
+    url: 'https://innovation-challenge.cloud.algorix.io/apply',
     images: [
       {
         url: '/og.png',
