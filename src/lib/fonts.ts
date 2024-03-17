be8859058552be4c7485f07721a8d3a9eb259b41
Mingeon Kim <issac4892@gmail.com>
2024-03-18 01:38:45 +0900

    font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,8 +2,10 @@ import localFont from 'next/font/local';
 
 export const wanted_sans = localFont({
   src: '../asset/fonts/WantedSansVariable.woff2',
+  display: 'swap',
 })
 
 export const happiness_sans = localFont({
   src: '../asset/fonts/HappinessSansVF.woff2',
+  display: 'swap',
 })
