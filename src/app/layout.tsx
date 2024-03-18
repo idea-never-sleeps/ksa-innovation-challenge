20f9227e7e3ee6a0cea70de5753703556102a4cc
Mingeon Kim <issac4892@gmail.com>
2024-03-18 23:07:13 +0900

    GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata, Viewport } from 'next';
 import { wanted_sans } from '@/lib/fonts';
 import StyledComponentsRegistry from '@/lib/registry';
-import './styles.css'
+import './styles.css';
 import 'normalize.css/normalize.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'KSA Innovation Challenge 2024',
@@ -26,8 +27,8 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#000000',
-}
+  themeColor: '#000000'
+};
 
 export default function RootLayout({
   children
@@ -38,6 +39,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className={wanted_sans.className} style={{ margin: 0, padding: 0 }}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <GoogleAnalytics gaId="G-S7W58FGP2P" />
       </body>
     </html>
   );
