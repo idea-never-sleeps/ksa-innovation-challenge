e2325dd58fa0a1cf51a22384bddcfdf2de104fbf
Mingeon Kim <issac4892@gmail.com>
2024-03-18 13:41:33 +0900

    font

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -73,214 +73,232 @@ export default function LandingPage() {
   }, [handleWheel, handleTouch]);
 
   return (
-    <RootContainer $scroll={scroll}>
-      <TopContainer>
-        <TopContent>
-          <TopTitle>
-            KSA
-            <br />
-            Innovation
-            <br />
-            Challenge
-          </TopTitle>
-          <div
-            style={{ position: 'relative', height: '600px', width: '100vw' }}
-          >
-            <LandingImage percentage={percentage} />
-          </div>
-          <BottomContainer>
-            <AssocationContainer>
-              <span className="title">주최</span>
-              <AssociationLogo>
-                <Image
-                  src="/ksa-black.svg"
-                  alt="KSA Logo"
-                  width="114"
-                  height="30"
-                />
-              </AssociationLogo>
-            </AssocationContainer>
-            <AssocationContainer>
-              <span className="title">주관</span>
-              <span className="content">2024 KIC 추진위원단</span>
-            </AssocationContainer>
-            <AssocationContainer>
-              <span className="title">후원</span>
-              <AssociationLogo>
-                <Image
-                  src="/kaist-black.svg"
-                  alt="KAIST Logo"
-                  width="85"
-                  height="24"
-                />
-                <Image
-                  src="/ict-black.svg"
-                  alt="과학기술정보통신부 Logo"
-                  width="124"
-                  height="24"
-                />
-              </AssociationLogo>
-            </AssocationContainer>
-          </BottomContainer>
-        </TopContent>
-      </TopContainer>
-      <ContentContainer ref={contentRef}>
-        <Section>
-          <TopTitle>
-            KSA
-            <br />
-            Innovation
-            <br />
-            Challenge
-          </TopTitle>
-          <div style={{ marginTop: '200px' }}>
-            <SectionTitle>What is KIC 2024?</SectionTitle>
-            <SectionContent>
-              KSA Innovation Challenge, KIC 2024는
+    <MainContainer>
+      <RootContainer
+        $scroll={scroll}
+        $height={contentRef.current?.offsetHeight || 0}
+      >
+        <TopContainer>
+          <TopContent>
+            <TopTitle>
+              KSA
               <br />
-              사회에 존재하는 <strong>꼭 해결해야 하는 문제</strong>를 위해
+              Innovation
               <br />
-              <br />
-              24시간 동안 팀을 이루어 솔루션을 찾아내는
-              <br />
-              <strong>아이디어톤</strong>입니다.
-              <br />
-              <br />
-              주제는 당일 공개될 예정입니다.
-            </SectionContent>
-          </div>
-        </Section>
-        <Section
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }}
-        >
-          <SectionTitle>시상</SectionTitle>
-          <AwardContainer>
-            <SectionLabel>1위</SectionLabel>
-            <SectionContent style={{textAlign: 'center'}}>KSA 학교장상 + 10만원 상당 상품</SectionContent>
-          </AwardContainer>
-          <AwardContainer>
-            <SectionLabel>2위</SectionLabel>
-            <SectionContent style={{textAlign: 'center'}}>
-              KSA 화학생물학부 학부장상 + 5만원 상당 상품
-            </SectionContent>
-          </AwardContainer>
-          <AwardContainer>
-            <SectionLabel>베스트 팀워크 상</SectionLabel>
-            <SectionContent style={{textAlign: 'center'}}>5만원 상당 상품</SectionContent>
-          </AwardContainer>
-        </Section>
-        <Section
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }}
-        >
-          <SectionTitle>참가자 특전</SectionTitle>
-          <AwardContainer>
-            <SectionLabel>웰컴 키트</SectionLabel>
-            <SectionContent>반팔 티셔츠, 스티커, 기타 굿즈</SectionContent>
-          </AwardContainer>
-          <AwardContainer>
-            <SectionLabel>다과</SectionLabel>
-            <SectionContent>무제한 몬스터, 탄산음료, 커피, 과자</SectionContent>
-          </AwardContainer>
-          <AwardContainer>
-            <SectionLabel>간식</SectionLabel>
-            <SectionContent>피자, 치킨 제공</SectionContent>
-          </AwardContainer>
-        </Section>
-        <Section style={{ marginBottom: '100px' }}>
-          <SectionTitle style={{ textAlign: 'center', lineHeight: '1.8' }}>
-            KIC,
-            <br />
-            Where Passion
-            <br />
-            Meets Purpose.
-            <br />
-            <br />
-            세상을 더 나은 곳으로 <br />
-            변화시킬 열정이 가득한
-            <br />
-            모든 분들의 지원을 기다립니다.
-          </SectionTitle>
-        </Section>
-        <Section>
-          <SectionContent
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column',
-              textAlign: 'center'
-            }}
-          >
-            KIC 조직위원단 드림.
-            <br />
-            <br />
-            22 권오윤
-            <br />
-            22 송시형
-            <br />
-            21 백동주
-            <br />
-            22 임우진
-            <br />
-            <br />
-            <br />
-            Website developed by
-            <br />
-            <strong>Mingeon Kim</strong>
-          </SectionContent>
-        </Section>
-        <Footer>
-          <FooterTitle>KIC : KSA Innovation Challenge</FooterTitle>
-          <ContactContainer>
-            <Contact>
-              <span>Email</span>
-              <span>ksa.hackathon.2024@gmail.com</span>
-            </Contact>
-            <Contact>
-              <span>카카오톡</span>
-              <span>22학번 권오윤 010-6650-4690</span>
-            </Contact>
-          </ContactContainer>
-          <FooterLink href="/privacy">개인정보 처리방침</FooterLink>
-          <FooterAssociationContainer>
-            <AssocationContainer>
-              <span className="title">주최</span>
-              <AssociationLogo>
-                <Image src="/ksa.svg" alt="KSA Logo" width="114" height="30" />
-              </AssociationLogo>
-            </AssocationContainer>
-            <AssocationContainer>
-              <span className="title">주관</span>
-              <span className="content">2024 KIC 추진위원단</span>
-            </AssocationContainer>
-            <AssocationContainer>
-              <span className="title">후원</span>
-              <AssociationLogo>
-                <Image
-                  src="/kaist.svg"
-                  alt="KAIST Logo"
-                  width="85"
-                  height="24"
-                />
-                <Image
-                  src="/ict.svg"
-                  alt="과학기술정보통신부 Logo"
-                  width="124"
-                  height="24"
-                />
-              </AssociationLogo>
-            </AssocationContainer>
-          </FooterAssociationContainer>
-        </Footer>
-      </ContentContainer>
-    </RootContainer>
+              Challenge
+            </TopTitle>
+            <div
+              style={{ position: 'relative', height: '600px', width: '100vw' }}
+            >
+              <LandingImage percentage={percentage} />
+            </div>
+            <BottomContainer>
+              <AssocationContainer>
+                <span className="title">주최</span>
+                <AssociationLogo>
+                  <Image
+                    src="/ksa-black.svg"
+                    alt="KSA Logo"
+                    width="114"
+                    height="30"
+                  />
+                </AssociationLogo>
+              </AssocationContainer>
+              <AssocationContainer>
+                <span className="title">주관</span>
+                <span className="content">2024 KIC 추진위원단</span>
+              </AssocationContainer>
+              <AssocationContainer>
+                <span className="title">후원</span>
+                <AssociationLogo>
+                  <Image
+                    src="/kaist-black.svg"
+                    alt="KAIST Logo"
+                    width="85"
+                    height="24"
+                  />
+                  <Image
+                    src="/ict-black.svg"
+                    alt="과학기술정보통신부 Logo"
+                    width="124"
+                    height="24"
+                  />
+                </AssociationLogo>
+              </AssocationContainer>
+            </BottomContainer>
+          </TopContent>
+        </TopContainer>
+        <ContentContainerContainer>
+          <ContentContainer ref={contentRef}>
+            <Section>
+              <TopTitle>
+                KSA
+                <br />
+                Innovation
+                <br />
+                Challenge
+              </TopTitle>
+              <div style={{ marginTop: '200px' }}>
+                <SectionTitle>What is KIC 2024?</SectionTitle>
+                <SectionContent>
+                  KSA Innovation Challenge, KIC 2024는
+                  <br />
+                  사회에 존재하는 <strong>꼭 해결해야 하는 문제</strong>를 위해
+                  <br />
+                  <br />
+                  24시간 동안 팀을 이루어 솔루션을 찾아내는
+                  <br />
+                  <strong>아이디어톤</strong>입니다.
+                  <br />
+                  <br />
+                  주제는 당일 공개될 예정입니다.
+                </SectionContent>
+              </div>
+            </Section>
+            <Section
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}
+            >
+              <SectionTitle>시상</SectionTitle>
+              <AwardContainer>
+                <SectionLabel>1위</SectionLabel>
+                <SectionContent style={{ textAlign: 'center' }}>
+                  KSA 학교장상 + 10만원 상당 상품
+                </SectionContent>
+              </AwardContainer>
+              <AwardContainer>
+                <SectionLabel>2위</SectionLabel>
+                <SectionContent style={{ textAlign: 'center' }}>
+                  KSA 화학생물학부 학부장상 + 5만원 상당 상품
+                </SectionContent>
+              </AwardContainer>
+              <AwardContainer>
+                <SectionLabel>베스트 팀워크 상</SectionLabel>
+                <SectionContent style={{ textAlign: 'center' }}>
+                  5만원 상당 상품
+                </SectionContent>
+              </AwardContainer>
+            </Section>
+            <Section
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}
+            >
+              <SectionTitle>참가자 특전</SectionTitle>
+              <AwardContainer>
+                <SectionLabel>웰컴 키트</SectionLabel>
+                <SectionContent>반팔 티셔츠, 스티커, 기타 굿즈</SectionContent>
+              </AwardContainer>
+              <AwardContainer>
+                <SectionLabel>다과</SectionLabel>
+                <SectionContent>
+                  무제한 몬스터, 탄산음료, 커피, 과자
+                </SectionContent>
+              </AwardContainer>
+              <AwardContainer>
+                <SectionLabel>간식</SectionLabel>
+                <SectionContent>피자, 치킨 제공</SectionContent>
+              </AwardContainer>
+            </Section>
+            <Section style={{ marginBottom: '100px' }}>
+              <SectionTitle style={{ textAlign: 'center', lineHeight: '1.8' }}>
+                KIC,
+                <br />
+                Where Passion
+                <br />
+                Meets Purpose.
+                <br />
+                <br />
+                세상을 더 나은 곳으로 <br />
+                변화시킬 열정이 가득한
+                <br />
+                모든 분들의 지원을 기다립니다.
+              </SectionTitle>
+            </Section>
+            <Section>
+              <SectionContent
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  textAlign: 'center'
+                }}
+              >
+                KIC 조직위원단 드림.
+                <br />
+                <br />
+                22 권오윤
+                <br />
+                22 송시형
+                <br />
+                21 백동주
+                <br />
+                22 임우진
+                <br />
+                <br />
+                <br />
+                Website developed by
+                <br />
+                <strong>Mingeon Kim</strong>
+              </SectionContent>
+            </Section>
+            <Footer>
+              <FooterTitle>KIC : KSA Innovation Challenge</FooterTitle>
+              <ContactContainer>
+                <Contact>
+                  <span>Email</span>
+                  <span>ksa.hackathon.2024@gmail.com</span>
+                </Contact>
+                <Contact>
+                  <span>카카오톡</span>
+                  <span>22학번 권오윤 010-6650-4690</span>
+                </Contact>
+              </ContactContainer>
+              <FooterLink href="/privacy">개인정보 처리방침</FooterLink>
+              <FooterAssociationContainer>
+                <AssocationContainer>
+                  <span className="title">주최</span>
+                  <AssociationLogo>
+                    <Image
+                      src="/ksa.svg"
+                      alt="KSA Logo"
+                      width="114"
+                      height="30"
+                    />
+                  </AssociationLogo>
+                </AssocationContainer>
+                <AssocationContainer>
+                  <span className="title">주관</span>
+                  <span className="content">2024 KIC 추진위원단</span>
+                </AssocationContainer>
+                <AssocationContainer>
+                  <span className="title">후원</span>
+                  <AssociationLogo>
+                    <Image
+                      src="/kaist.svg"
+                      alt="KAIST Logo"
+                      width="85"
+                      height="24"
+                    />
+                    <Image
+                      src="/ict.svg"
+                      alt="과학기술정보통신부 Logo"
+                      width="124"
+                      height="24"
+                    />
+                  </AssociationLogo>
+                </AssocationContainer>
+              </FooterAssociationContainer>
+            </Footer>
+          </ContentContainer>
+        </ContentContainerContainer>
+      </RootContainer>
+    </MainContainer>
   );
 }
 
@@ -288,13 +306,12 @@ const MainContainer = styled.div`
   height: 100vh;
   height: 100svh !important;
   overflow-y: scroll;
-  position: relative;
   letter-spacing: -4%;
 `;
 
-const RootContainer = styled.div<{ $scroll: boolean }>`
+const RootContainer = styled.div<{ $scroll: boolean; $height: number }>`
+  height: ${({ $scroll, $height }) => ($scroll ? `calc(100svh + ${$height}px)` : '100svh')};
   overflow: hidden;
-  height: ${({ $scroll }) => ($scroll ? 'auto' : '100svh')};
   position: relative;
 `;
 
@@ -304,7 +321,7 @@ const TopContainer = styled.div`
   position: fixed;
   top: 0;
   height: 100vh;
-  height: 100dvh !important;
+  height: 100svh !important;
   width: 100vw;
   color: #000;
   background: #fff;
@@ -313,7 +330,7 @@ const TopContainer = styled.div`
 const TopContent = styled.div`
   box-sizing: border-box;
   height: 100vh;
-  height: 100dvh !important;
+  height: 100svh !important;
   width: 100vw;
   display: flex;
   flex-direction: column;
@@ -388,14 +405,23 @@ const AssociationLogo = styled.div`
   gap: 10px;
 `;
 
-const ContentContainer = styled.div`
+const ContentContainerContainer = styled.div`
   z-index: 101;
   position: relative;
   margin-top: 100vh;
-  margin-top: 100dvh !important;
+  margin-top: 100svh !important;
+  display: flex;
+  justify-content: center;
+  background: #1e1e1e;
+`;
+
+const ContentContainer = styled.div`
+  z-index: 101;
+  position: relative;
   padding: 20px;
   background: #1e1e1e;
   color: #fff;
+  max-width: 1000px;
 `;
 
 const Section = styled.div`
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,11 +1,61 @@
 import localFont from 'next/font/local';
 
 export const wanted_sans = localFont({
-  src: '../asset/fonts/WantedSansVariable.woff2',
-  display: 'swap',
+  src: [
+    {
+      path: '../asset/fonts/wanted/WantedSans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/wanted/WantedSans-ExtraBlack.woff2',
+      weight: '1000',
+      style: 'normal',
+    },
+  ],
 })
 
 export const happiness_sans = localFont({
-  src: '../asset/fonts/HappinessSansVF.woff2',
-  display: 'swap',
+  src: [
+    {
+      path: '../asset/fonts/happiness/Happiness-Sans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/happiness/Happiness-Sans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../asset/fonts/happiness/Happiness-Sans-Title.woff2',
+      weight: '900',
+      style: 'normal',
+    }
+  ],
 })
